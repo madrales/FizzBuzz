52a0a8aca352066e7aea9ecbd78aef9d70c2702b
Mason Adrales <masonadrales12@gmail.com>
2019-01-23 23:17:50 -0600

    Fixed the range which included 0 initially

--- FB.py
+++ FB.py
@@ -1,6 +1,6 @@
 #FizzBuzz, multiples of 3 are Fizz and multiples of 5 are Buzz
 #multiples of both are FizzBuzz and multiples or neither are just the number
-for i in range(0, 101):
+for i in range(1, 101):
 	if i % 3 == 0 and i % 5 != 0:
 		print("Fizz")
 	elif i % 3 != 0 and i % 5 == 0:
